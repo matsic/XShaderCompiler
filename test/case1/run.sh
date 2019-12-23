d76bdb45601f5d3ec40b0fdd3c9366f8e4c0dccb
matsic <matsybora@gmail.com>
2019-12-23 19:37:43 +0200

    init case1 change preprocessor to gcc

---
+++ test/case1/run.sh
@@ -0,0 +1,9 @@
+XSC=../../build/xsc
+
+#../../build/xsc --comments ON --reflect ON --show-ast ON -T frag -E main --version-in HLSL5 --version-out GLSL450 -D__NVN__ Fxaa.psh
+#../../build/xsc -PP -T frag -E main --version-in HLSL5 --version-out GLSL450 -D__NVN__ Fxaa.psh -o fxaa.pp
+
+#OPT="--comments ON --reflect ON --show-ast ON"
+OPT=""
+
+gcc -x c -D__NVN__ -E Fxaa.psh -o fxaa.pp.psh && $XSC $OPT -T frag -E main --version-in HLSL5 --version-out GLSL450 -D__NVN__ fxaa.pp.psh
